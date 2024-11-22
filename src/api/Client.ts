0a1efbbc33c62c4cf3fd9358b925e12153f07dc9
fevico <ajayifavour81@gmail.com>
2024-11-22 11:14:21 +0100

    added backend server ip

--- src/api/Client.ts
+++ src/api/Client.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const client = axios.create({
-    baseURL: "http://localhost:8000"
+    baseURL: "https://e-book-server-399n.onrender.com"
 });
 
 client.interceptors.request.use(
